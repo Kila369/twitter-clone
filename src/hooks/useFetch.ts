483e98325f59cf8a9cefb3db5867645a20e9c55d
Kila369 <lazar.joksimovic7@gmail.com>
2024-11-04 14:47:14 +0100

    Create useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}${url}`);
+        console.log(import.meta.env.VITE_API_BASE_URL)
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result: T = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error instanceof Error ? error : new Error('Something went wrong'));
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
