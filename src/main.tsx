501f34a58ca6550fd9301ee7ab79de0f81cd2ee9
Kila369 <lazar.joksimovic7@gmail.com>
2024-11-04 14:46:50 +0100

    Add route for feed in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import Feed from './routes/Feed.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/feed",
+    element: <Feed />,
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
